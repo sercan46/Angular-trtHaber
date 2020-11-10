bc4ad5789395b7dc334d695c2232bedd08254e2c
sercan46 <54890232+sercan46@users.noreply.github.com>
2020-11-10 17:30:15 +0300

    trt web site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/anasayfa', pathMatch: 'full'},
+  {path:'anasayfa', loadChildren: () => import('./anasayfa/anasayfa.module').then(m => m.AnasayfaModule)},
+  {path:'spor', loadChildren: () => import('./spor/spor.module').then(m => m.SporModule)},
+  {path:'ekonomi', loadChildren: () => import('./ekonomi/ekonomi.module').then(m => m.EkonomiModule)},
+  {path:'saglik', loadChildren: () => import('./saglik/saglik.module').then(m => m.SaglikModule)},
+  {path:'egitim', loadChildren: () => import('./egitim/egitim.module').then(m => m.EgitimModule)},
+  {path:'kultur-sanat', loadChildren: () => import('./sanat/sanat.module').then(m => m.SanatModule)},
+  {path:'haber-detay/:id',loadChildren: () => import('./haberdetay/haberdetay.module').then(m => m.HaberdetayModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+
 })
 export class AppModule { }
